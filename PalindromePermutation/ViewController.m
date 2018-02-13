c70c8c803116c2b9b0cf950bec78ea0d4c12fc04
Venkata Narasimham Peetla <venkata.n.peetla@kp.org>
2018-02-12 23:06:08 -0800

    Function that checks whether any permutation of an input string is a palindrome.

--- PalindromePermutation/ViewController.m
+++ PalindromePermutation/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    NSLog(@"Civic %d", HasPalindromePermutation(@"civic"));
+
 }
 
 
@@ -25,5 +28,27 @@
     // Dispose of any resources that can be recreated.
 }
 
+//Complexity : O(n) time, space complexity is O(1).
+BOOL HasPalindromePermutation(NSString *str) {
+
+    // track characters we've seen an odd number of times
+    NSMutableSet<NSString *> *unpairedCharacters = [NSMutableSet new];
+
+    // for each character in str
+    for (NSUInteger i = 0; i < str.length; i++) {
+        NSString *c = [str substringWithRange:NSMakeRange(i, 1)];
+
+        if ([unpairedCharacters containsObject:c]) {
+            [unpairedCharacters removeObject:c];
+        } else {
+            [unpairedCharacters addObject:c];
+        }
+    }
+
+    // the string has a palindrome permutation if it
+    // has one or zero characters without a pair
+    return (unpairedCharacters.count <= 1);
+
+}
 
 @end
